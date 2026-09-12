9e5b302a1fd22ae29e12274fe9a9476b37eceaa1
sami4290 <sami261419@gmail.com>
2026-09-12 17:49:14 +0100

    Add files via upload

--- case-images.js
+++ case-images.js
@@ -1,7 +1,7 @@
 // Maps each investigation case title to a representative image.
 const CASE_IMAGES = {
   "The Vanishing Diamond": "case-diamond.jpg",
-  "The Silenced Solo": "case-violin.jpg",
+  "The Silenced Solo": "case-violin2.jpg",
   "The Silenced Bell": "case-bell.jpg",
   "The Forged Signature": "case-art.jpg",
   "The Empty Reliquary": "case-chapel.jpg",
